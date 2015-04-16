d8ba6f130637874f4f621338e6e4e9c159033ae2
torggaman <torggaman@hotmail.com>
2015-04-16 00:00:46 -0700

    Updated Destiny.py
    Added some mock shops in the tower
    Weapons and Armor
    (Hawkmoon on PC before it is on Xbox)
    Along with figured out how to manipulate the dictionary that I am using
    for the commands

--- destiny.py
+++ destiny.py
@@ -52,56 +52,67 @@ class player(character):
             elif self.job == "Hunter":
                 self.subjob = input("Choose a Subclass 'Gunslinger' or 'Bladedancer': ")
             self.made += 1
+            del Cmd['create character']
         else:
             print("You already have a character.")
 
     def help(self):
         print(Cmd.keys())
 
-    def travel(self):
+    def orbit(self):
         if self.made == 1:
             if self.destination != "Space":
-                answer = input("Would you like to go to space? \n 'yes' or 'no': ")
-                if answer == "yes":
+                print("Do you want to return to orbit?")
+                goorbit = input("'yes' or 'no': ")
+                if goorbit == "yes":
                     print("Your ship gracefully swoops down and picks you up.")
                     self.destination = "Space"
-                else:
-                    print("invalid selection")
-            elif self.destination == "Space":
+                    if Cmd.keys() == 'shop':
+                            del Cmd['shop']
+            else:
+                print("You are already in orbit")
+        else:
+            print("Make a character first.")
+
+    def travel(self):
+        if self.made == 1:
+            if self.destination == "Space":
                 answer = input("Where would you like to go? \n %s: " % planets)
-                if answer == "Earth":
+                if answer == "earth":
                     self.destination = "Earth"
                     self.currentmap = ""
                     print("Initiating Warp Drive")
                     print("Welcome to Earth")
-                elif answer == "Tower":
+                elif answer == "tower":
                     self.destination = "Tower"
                     self.currentmap = ""
+                    Cmd['shop'] = player.shop
                     print("Initiating Warp Drive")
                     print("Welcome to the Tower")
-                elif answer == "Moon":
+                elif answer == "moon":
                     self.destination = "Moon"
                     self.currentmap = ""
                     print("Initiating Warp Drive")
                     print("Welcome to the Moon")
-                elif answer == "Venus":
+                elif answer == "venus":
                     self.destination = "Venus"
                     self.currentmap = ""
                     print("Initiating Warp Drive")
                     print("Welcome to Venus")
-                elif answer == "Mars":
+                elif answer == "mars":
                     self.destination = "Mars"
                     self.currentmap = ""
                     print("Initiating Warp Drive")
                     print("Welcome to Mars")
                 else:
-                    print("Invalid destination")
-                    player.travel
+                    print(invalidinput)
         else:
             print("Make a character first.")
             
     def inventory(self):
         print(inventory2)
+        print("%s glimmer" % self.glimmer)
+        print("%s vanguard marks" % self.vanguardmarks)
 
     def equip(self):
         if self.made != 1:
@@ -137,52 +148,65 @@ class player(character):
             print("Make a character first.")
         else:
             if self.destination == "Tower":
-                print(towerd)
-                line = input("> ")
-                args = line.split()
-                if len(args) > 0:
-                    commandFound = False
-                    for c in towerd.keys():
-                        if args[0] == c[:len(args[0])]:
-                            towerd[c](p)
-                            commandFound = True
-                            break
-                    if not commandFound:
-                        print("%s doesn't understand the suggestion." % p.name)
+                print("Looks like there are some shops to visit")
             elif self.destination == "Earth":
                 print("Lets explore Earth!")
             else:
                 print("travel to another destination.")
 
     def shop(self):
-        if self.glimmer == 0:
+        if self.glimmer >= 0:
             print("What would you like?")
-            shopping = input("Weapons or Armor: ")
+            shopping = input("weapons or armor: ")
+            if shopping == "weapons":
+                print(weaponshop)
+                purchase1 = input("Please choose a weapon: ")
+                if self.glimmer >= weaponshop[purchase1]:
+                    self.glimmer -= weaponshop[purchase1]
+                    weaponinv.append(purchase1)
+                    print("Thank you for shopping")
+                else:
+                    print("You do not have enough glimmer for %s" % purchase1)
+            elif shopping == "armor":
+                print(armorshop)
+                purchase = input("Please choose a armor set: ")
+                if self.glimmer >= armorshop[purchase]:
+                    self.glimmer -= armorshop[purchase]
+                    armorinv.append(purchase)
+                else:
+                    print("You do not have enough glimmer for %s" % purchase)
+        else:
+            print("You should gather more glimmer")
+
+    def giveglimmer(self):
+        print("How much glimmer do you want? ")
+        glimmer2 = input()
+        self.glimmer += int(glimmer2)
+        print("%s glimmer has been added to your account" % glimmer2)
 
     def view(self):
         if self.destination == "Tower":
-            print("You look around the Tower.")
+            print("You look at the view from the Tower.")
 
-towerd = {
-    "shop": player.shop,
-    "view": player.view,
-    }
+invalidinput = print("Invalid input")
 
+race = {"human", "awoken", "exo", }
 
-race = {"Human", "Awoken", "Exo", }
+job = {"titan", "hunter", "warlock", }
 
-job = {"Titan", "Hunter", "Warlock", }
-
-inventory2 = ["item 1", "item 2", ]
+inventory2 = []
 
 weaponinv = ["Starter Hand Cannon", ]
 
 armorinv = ["Starter Set", ]
 
-planets = ["Tower", "Earth", "Moon", "Venus", "Mars", ]
+planets = ["tower", "earth", "moon", "venus", "mars", ]
+
+weaponshop = {'Hawkmoon': 2000, }
+
+armorshop = {'VoG set': 4000, }
 
 p = player()
-
 
 print("Welcome to Destiny")
 print("Connecting to Destiny servers...")
@@ -197,6 +221,8 @@ Cmd = {
     "inventory": player.inventory,
     "equip": player.equip,
     "explore": player.explore,
+    "give glimmer": player.giveglimmer,
+    "go to orbit": player.orbit,
     }
 
 
